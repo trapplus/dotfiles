efca0f7d367bc08aad5833a0be29e0f19a13b7e8
Eviltrapgod <blazer19092008@gmail.com>
2026-03-21 18:56:03 +0300

    Delete scripts directory

--- scripts/container.py
+++
@@ -1,7 +0,0 @@
-from scripts.utils.dload import downloadController
-from scripts.utils.files import fileController
-from scripts.utils.shell import ShellController
-
-shell = ShellController()
-loads = downloadController()
-files = fileController()
--- scripts/controllers/fastfetch.py
+++
@@ -1,3 +0,0 @@
-class fastfetch:
-    def __init__(self) -> None:
-        pass
